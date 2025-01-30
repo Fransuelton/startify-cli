291802b1fa0fe502d99dcb998b584dd3b4049f54
elton <elton6103@gmail.com>
2025-01-30 10:43:26 -0300

    ✨ feat: add interactive CLI prompts for project setup

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 #!/usr/bin/env node
 
-console.log("Olá Mundo! Este é meu primeiro CLI!");
+import inquirer from "inquirer";
+import chalk from "chalk";
 
+async function ask() {
+  const response = await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Qual o nome do seu projeto?",
+      default: "meu-projeto",
+    },
+    {
+      type: "list",
+      name: "type",
+      message: "Qual o tipo de projeto você deseja criar?",
+      choices: ["Web"],
+    },
+    {
+      type: "confirm",
+      name: "InstallDependencies",
+      message: "Deseja instalar dependências automaticamente?",
+      default: true,
+    },
+  ]);
+
+  console.log(
+    chalk.green(
+      `Projeto "${response.name}" de tipo "${response.type}" criado com sucesso!`
+    )
+  );
+
+  if (response.InstallDependencies) {
+    // Lógica para instalar dependências aqui
+  }
+}
+
+ask();
